65c550c445a9f2ded8adac7c3ccd948dc53a178e
Cameron Kelley <i.am.cjk@gmail.com>
2015-07-15 15:18:33 -0400

    Tweaks

--- app/js/controllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var babbitApp = angular.module("babbitApp", []);
-
-babbitApp.controller("CarCtrl", ["$scope", "$http", function($scope, $http) {
-	$http.get("cars.json").success(function(data) {
-		$scope.cars = data;
-	})
-});
