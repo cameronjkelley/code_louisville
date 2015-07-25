a0035648b857699b58699335933735e96065c3f8
Cameron Kelley <i.am.cjk@gmail.com>
2015-07-25 16:32:15 -0400

    Deleted App folder

--- app/js/controllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var babbitAppControllers = angular.module("babbitAppControllers", []);
-
-babbitAppControllers.controller("CarCtrl", ["$scope", "$http", function($scope, $http) {
-	$http.get("../cars.json").success(function(data) {
-		$scope.cars = data;
-	});
-}]);
