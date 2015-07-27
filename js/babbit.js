9d3dba43a30087f5d78d2f2a4070247681904218
Cameron Kelley <i.am.cjk@gmail.com>
2015-07-26 21:49:18 -0400

    CSS tweaks

---
+++ js/babbit.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	var $video = $('video');
+	
+	$video.mouseenter(function() {
+		$(this).get(0).pause();
+	});
+
+	$video.mouseleave(function() {
+		$(this).get(0).play();
+	});
+
+	// if ($(window).width() <= 768) {
+	// 	$(".slick").slick({
+	// 		infinite: true
+	// 	});
+	// }
+
+	// $(window).resize(function() {
+	// 	if ($(window).width() <= 768) {
+	// 		$(".slick").slick({
+	// 			infinite: true
+	// 		});
+	// 	}
+	// });
+
+});
