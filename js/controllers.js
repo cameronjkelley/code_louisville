08d92b94e0b3e7a9892017a2ab52f8add0e3312a
Cameron Kelley <i.am.cjk@gmail.com>
2015-07-31 23:25:01 -0400

    Route fix

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,17 @@
 "use strict";
 
-var babbitAppControllers = angular.module("babbitAppControllers", []);
+var babbitAppControllers = angular.module("babbitAppControllers", [])
 
-babbitAppControllers.controller("CarCtrl", ["$scope", "$http", "$routeParams", 
-	function($scope, $http, $routeParams) {
+.controller("CarCtrl", ["$scope", "$http", "$location", "$routeParams", 
+	function($scope, $http, $location, $routeParams) {
+		$scope.carId = $routeParams.carId;
+		$scope.listing = [];
 		$http.get("cars.json").success(function(data) {
 			$scope.cars = data;
-	});
-		$scope.carId = $routeParams.carId;
+			for (var i = 0; i < $scope.cars.length; i ++) {
+				if ($scope.cars[i].id === $scope.carId) {
+					$scope.listing.push($scope.cars[i]);
+				}
+			}
+		});
 	}]);
